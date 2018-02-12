e547639d7f80fa7737f481f58f079755c9d72d1c
firsttris <tristanteufel@googlemail.com>
2018-02-12 21:57:20 +0100

    (feature) update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,33 @@
-const path = require('path');
+const path = require('path'),
+  webpack = require('webpack'),
+  cleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    target: 'node',
-    context: __dirname,
-    entry: ["./src/index.js"],
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "index.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
-                    }
-                }
-            }
-        ]
-    },
-    plugins: [
-        new cleanWebpackPlugin(['dist']),
-        new webpack.DefinePlugin({
-          'process.env.NODE_ENV': JSON.stringify('production')
-        })
-      ],
+  target: "node",
+  context: __dirname,
+  entry: ["./src/index.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new cleanWebpackPlugin(["dist"]),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("production")
+    })
+  ]
 };
